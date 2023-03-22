4d547d757124901c62435e64d65c069b483e0606
孤之界 <f20001012@gmail.com>
2023-03-22 11:50:13 +0800

    add: SolutionItems

---
+++ DiscordChatGPTBot/Interaction/OpenAI/Service/OpenAIService.cs
@@ -0,0 +1,19 @@
+﻿using OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordChatGPTBot.Interaction.OpenAI.Service
+{
+    public class OpenAIService : IInteractionService
+    {
+        private readonly OpenAIClient _openAIClient;
+
+        public OpenAIService(BotConfig botConfig)
+        {
+            _openAIClient = new OpenAIClient(new OpenAIAuthentication(botConfig.OpenAIToken));
+        }
+    }
+}
