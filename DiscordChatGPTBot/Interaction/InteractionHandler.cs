e2523f6f496e917742bfc9454ae0fc846107a614
孤之界 <f20001012@gmail.com>
2023-03-24 11:32:44 +0800

    fix: `/say` 指令不會記錄 message 的錯誤

--- DiscordChatGPTBot/Interaction/InteractionHandler.cs
+++ DiscordChatGPTBot/Interaction/InteractionHandler.cs
@@ -118,6 +118,9 @@ namespace DiscordChatGPTBot.Interaction
         {
             try
             {
+                if (socketSlashCommandDataOption.Type != ApplicationCommandOptionType.SubCommand && socketSlashCommandDataOption.Type != ApplicationCommandOptionType.SubCommandGroup && !socketSlashCommandDataOption.Options.Any())
+                    return $" {socketSlashCommandDataOption.Value}";
+
                 if (socketSlashCommandDataOption.Type == ApplicationCommandOptionType.SubCommand || socketSlashCommandDataOption.Type == ApplicationCommandOptionType.SubCommandGroup) GetOptionsValue(socketSlashCommandDataOption.Options.First());
                 return " " + string.Join(' ', socketSlashCommandDataOption.Options.Select(option => option.Value));
             }
